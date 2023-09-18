168863b54bf843003641cb2c80cc68d6dd100a0d
Mohammad Saqib <m.saqib@student.uw.edu.pl>
2023-09-18 09:15:47 +0200

    energy data header name fixed.

--- LeapFrogCppUcsdEdu_10/LeapFrogCppUcsdEdu10__GTest/header.h
+++ LeapFrogCppUcsdEdu_10/LeapFrogCppUcsdEdu10__GTest/header.h
@@ -4,7 +4,7 @@
 #define HEADER_H
 
 #include "../LeapFrogCppUcsdEdu_10/common.hpp"
-#include "../LeapFrogCppUcsdEdu_10/energy.hpp"
+#include "../LeapFrogCppUcsdEdu_10/energy_data.hpp"
 #include "../LeapFrogCppUcsdEdu_10/file_io.hpp"
 #include "../LeapFrogCppUcsdEdu_10/integrator.hpp"
 #include "../LeapFrogCppUcsdEdu_10/lennard_jones.hpp"
