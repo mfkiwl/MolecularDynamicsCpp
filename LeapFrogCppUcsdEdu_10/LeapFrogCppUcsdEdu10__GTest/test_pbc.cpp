2f66f44b4a0aac514a41af9785b69419a2479a3a
Mohammad Saqib <m.saqib@student.uw.edu.pl>
2023-09-21 13:04:06 +0200

    periodic boundary condition fixed

--- LeapFrogCppUcsdEdu_10/LeapFrogCppUcsdEdu10__GTest/test_pbc.cpp
+++ LeapFrogCppUcsdEdu_10/LeapFrogCppUcsdEdu10__GTest/test_pbc.cpp
@@ -5,15 +5,17 @@
 
 TEST(TestPBC, TestApply)
 {
-	real rCutOff = 0.9;
-	real neighborRadius = 90;
+	PBC::boxLength = 90;
 
-	Vec3 position1(-90, 90, 45);
-	Vec3 position2(90, -45, 45);
+	Vec3 position1(45, 45, 45); PBC::Apply(position1);
+	Vec3 position2(90, 90, 90); PBC::Apply(position2);
+	Vec3 position3(-90, -90, -90); PBC::Apply(position3);
+	Vec3 position4(0, 0, 0); PBC::Apply(position4);
+	Vec3 position5(-1, -1, -1); PBC::Apply(position5);
 
-	PBC::Apply(position1);
-	PBC::Apply(position2);
-
-	EXPECT_EQ(true, Vec3(-84, 84, 39) == position1);
-	EXPECT_EQ(true, Vec3(84, -39, 39) == position2);
+	EXPECT_EQ(true, Vec3(45, 45, 45) == position1);
+	EXPECT_EQ(true, Vec3(0, 0, 0) == position2);
+	EXPECT_EQ(true, Vec3(0, 0, 0) == position3);
+	EXPECT_EQ(true, Vec3(0, 0, 0) == position4);
+	EXPECT_EQ(true, Vec3(89, 89, 89) == position5);
 }
--- LeapFrogCppUcsdEdu_10/LeapFrogCppUcsdEdu_10/pbc.hpp
+++ LeapFrogCppUcsdEdu_10/LeapFrogCppUcsdEdu_10/pbc.hpp
@@ -3,34 +3,22 @@
 #include "common.hpp"
 #include "vec3.hpp"
 
-/*
-double PeriodicBoundaryCondition(double value)
-{
-	if (value > 17)
-		value -= 34;
-	if (value < -17)
-		value += 34;
-
-	return value;
-}
-*/
-
-class PBC //Periodic Boundary Condition
+class PBC
 {
 public:
-	 static real boxLength;
+	static real boxLength;
 
-	static void Apply(real & value) 
-	{
-		real boxLengthHalf = boxLength / 2.0;
-
-		if (value > boxLengthHalf)
-			value -= boxLength;
-		if (value < -boxLengthHalf)
+	// Applies the periodic boundary condition to a single value
+	static void Apply(real& value) {
+		if (value < 0) {
 			value += boxLength;
+		}
+		else if (value >= boxLength) {
+			value -= boxLength;
+		}
 	}
 
-	static void Apply(Vec3 & vec)
+	static void Apply(Vec3& vec)
 	{
 		Apply(vec.x);
 		Apply(vec.y);
