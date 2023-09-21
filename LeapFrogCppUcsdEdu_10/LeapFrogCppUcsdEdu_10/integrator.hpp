55501ef7996a746ee360464cb2d2d4687535f8f7
Mohammad Saqib <m.saqib@student.uw.edu.pl>
2023-09-21 20:56:00 +0200

    leapfrog

--- LeapFrogCppUcsdEdu_10/LeapFrogCppUcsdEdu_10/integrator.hpp
+++ LeapFrogCppUcsdEdu_10/LeapFrogCppUcsdEdu_10/integrator.hpp
@@ -46,49 +46,6 @@ public:
 		update_velocity(particles_, dt);
 		update_position(particles_, 0.5 * dt);
 	}
-
-	// http://www.artcompsci.org/vol_1/v1_web/node34.html
-	//static void Lepfrog(std::vector<Particle>& particles_, double dt)
-	//{
-	//	int n_particles = particles_.size();
-	//
-	//	// Compute acceleration and half velocity step for all particles
-	//	for (int i = 0; i < n_particles; i++)
-	//	{
-	//		Particle& particle = particles_[i];
-	//		
-	//		Vec3 acceleration;
-	//		for (int j = 0; j < n_particles; j++)
-	//		{
-	//			if (i != j)
-	//			{
-	//				Particle& other = particles_[j];
-	//				acceleration += particle.getAcceleration(other);
-	//				particle.velocity += 0.5 * dt * acceleration; // update velocity by half step
-	//			}
-	//		}			
-	//	}
-	//
-	//	// Update position and another half velocity step for all particles
-	//	for (int i = 0; i < n_particles; i++)
-	//	{
-	//		Particle& particle = particles_[i];
-	//		particle.position += dt * particle.velocity; // update position by full step
-	//
-	//		PBC::Apply(particle.position); //apply periodic boundary condition
-	//
-	//		Vec3 acceleration;
-	//		for (int j = 0; j < n_particles; j++)
-	//		{
-	//			if (i != j)
-	//			{
-	//				Particle& other = particles_[j];
-	//				acceleration = particle.getAcceleration(other);
-	//				particle.velocity += 0.5 * dt * acceleration; // update velocity by half step
-	//			}
-	//		}			
-	//	}
-	//}
 };
 
 #endif // !INTEGRATOR_HPP
