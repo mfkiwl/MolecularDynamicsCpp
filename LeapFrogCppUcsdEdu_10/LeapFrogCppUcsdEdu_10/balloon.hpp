ce808c38ad18b86ae0d1c968fc8911a8235640a1
Mohammad Saqib <m.saqib@student.uw.edu.pl>
2023-09-22 11:47:13 +0200

    Requirments.pdf updated

---
+++ LeapFrogCppUcsdEdu_10/LeapFrogCppUcsdEdu_10/balloon.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "common.hpp"
+#include "vec3.hpp"
+
+class Baloon
+{
+public:
+	static real boxLength;
+
+	// Applies the periodic boundary condition to a single value
+	static void Apply(real& value)
+	{
+		if (value < 0)
+		{
+			value += boxLength;
+		}
+		else if (value >= boxLength)
+		{
+			value -= boxLength;
+		}
+	}
+
+	static void Apply(Vec3& vec)
+	{
+		Apply(vec.x);
+		Apply(vec.y);
+		Apply(vec.z);
+	}
+};
+
